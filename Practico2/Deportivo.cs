d90f502865a9c083673ced7f596c5712193e5797
Karen Fernandez <145879499+KarenFernandez13@users.noreply.github.com>
2023-11-02 21:22:35 -0300

    Add files via upload

---
+++ Practico2/Deportivo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico2
+{
+    internal class Deportivo: Vehiculo
+    {
+        private int velocidadMaxima;
+        public Deportivo(int numero, string matricula, string marca, string color, int capacidadTanque, bool disponibilidad, double precioAlquilerDia, int kmLitro, int velocidadMaxima) : base(numero, matricula, marca, color, capacidadTanque, disponibilidad, precioAlquilerDia, kmLitro)
+        {
+            this.velocidadMaxima = velocidadMaxima;
+        }
+        public int GetVelocidadMaxima() => velocidadMaxima;
+        public void SetVelocidadMaxima(int velocidadMaxima) => this.velocidadMaxima = velocidadMaxima;
+    }
+}
---
+++ Practico2/Sucursal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico2
+{
+    internal class Sucursal
+    {
+        private int numero;
+        private string direccion;
+        private List<Alquiler> colAlquileres;
+        private List<Vehiculo> colVehiculos;
+
+        public Sucursal(int numero, string direccion, List<Alquiler> colAlquileres, List<Vehiculo> colVehiculos)
+        {
+            this.numero = numero;
+            this.direccion = direccion;
+            this.colAlquileres = colAlquileres;
+            this.colVehiculos = colVehiculos;
+        }
+        public int GetNumero() => numero; 
+        public string GetDireccion() => direccion;
+        public List<Alquiler> GetColAlquileres() => colAlquileres;
+        public List<Vehiculo> GetColVehiculos() => colVehiculos;
+        
+        public void SetNumero(int numero) => this.numero = numero;
+        public void SetDireccion(string direccion) => this.direccion = direccion;
+        public void SetColAlquileres(List<Alquiler> colAlquileres) => this.colAlquileres = colAlquileres;
+        public void SetColVehiculos(List<Vehiculo> colVehiculos) => this.colVehiculos = colVehiculos;
+        
+        public string VehiculosStock() 
+        {
+            foreach (Vehiculo item in colVehiculos)
+            {
+                return item.GetMarca();
+            }
+            return "";
+        }
+    }
+}
